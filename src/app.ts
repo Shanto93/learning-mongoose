17bb1e86587a7a1bb372ea7deaf64f8fea14162b
Shanto93 <shantoislam7363@gmail.com>
2025-06-15 17:01:20 +0600

    POST notes using Schema

--- src/app.ts
+++ src/app.ts
@@ -3,23 +3,49 @@ import mongoose, { Schema } from "mongoose";
 
 const app: Application = express();
 
+app.use(express.json());
+
 const noteSchema = new mongoose.Schema({
-  title: String,
-  content: String,
+  title: { type: String, require: true, trim: true },
+  content: { type: String, default: "" },
+  categories: {
+    type: String,
+    enum: ["personal", "work", "study", "others"],
+    default: "personal",
+  },
+  pinned: {
+    type: Boolean,
+    default: false,
+  },
+  tags: {
+    label: { type: String, require: true },
+    color: { type: String, default: "gray" },
+  },
 });
 
 const Note = mongoose.model("Note", noteSchema);
 
-app.post("/create-note", async (req: Request, res: Response) => {
-  const myNote = new Note({
-    title: "Learning Mongoose",
-    content: "I am learning Mogoose",
-  });
-  await myNote.save();
+app.post("/note/create-note", async (req: Request, res: Response) => {
+  const body = req.body;
+
+  //Approach-1
+  // const myNote = new Note({
+  //   title: "Learning Express",
+  //   content: "I am learning Express",
+  //   tags:{
+  //     label: "Express",
+  //     color: "red"
+  //   }
+  // });
+  // await myNote.save();
+
+  //Approach-2
+  const note = await Note.create(body);
+
   res.status(201).json({
     success: true,
     message: "Note created successfully",
-    note: myNote,
+    note: note,
   });
 });
 
