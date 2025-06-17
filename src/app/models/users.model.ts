a1e1109e6e98efe25075954132917e511866a377
Shanto93 <shantoislam7363@gmail.com>
2025-06-17 10:56:40 +0600

    Refine user model by adding some extra custom validator

--- src/app/models/users.model.ts
+++ src/app/models/users.model.ts
@@ -5,37 +5,53 @@ const userSchema = new mongoose.Schema<IUser>(
   {
     firstName: {
       type: String,
-      require: true,
+      require: [true, "First Name is required"],
       trim: true,
       maxlength: [50, "First Name should be maximum 50 characters"],
     },
     lastName: {
       type: String,
-      require: true,
+      require: [true, "Last Name is required."],
       trim: true,
       maxlength: [50, "Last Name should be maximum 50 characters"],
     },
     age: {
       type: Number,
-      required: true,
+      required: [true, "Age is required"],
       max: [60, "Age should be maximum 60"],
       min: [18, "Age should be maximum 60"],
     },
     email: {
       type: String,
-      require: true,
-      unique: true,
+      require: [true, "Email is required"],
+      unique: [true, "Email should be unique"],
       lowercase: true,
       trim: true,
+      validate: {
+        validator: function (v) {
+          return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+        },
+        message: "Invalid email address",
+      },
     },
     password: {
       type: String,
-      require: true,
+      require: [true, "Password is required."],
+      validate: {
+        validator: function (pass) {
+          return /^(?=.*[A-Z])(?=.*\d).+$/.test(pass);
+        },
+        message:
+          "Password must contain at least one uppercase letter and one number",
+      },
     },
     role: {
       type: String,
       lowercase: true,
-      enum: ["admin", "user"],
+      enum: {
+        values: ["admin", "user"],
+        message: "Role is not valid. got {VALUE} role",
+      },
       default: "user",
     },
   },
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import type { Server } from "http";
 import app from "./app";
 import mongoose from "mongoose";
+import { User } from "./app/models/users.model";
 
 let server: Server;
 const port = 5000;
@@ -9,6 +10,10 @@ async function main() {
     await mongoose.connect(
       "mongodb+srv://mongooseDB:jiK4ds7FppK1izJc@cluster0.tuf9wrv.mongodb.net/advance-note-app?retryWrites=true&w=majority&appName=Cluster0"
     );
+    // const result = await User.updateMany(
+    //   { age: { $exists: false } },
+    //   { $set: { age: 30 } }
+    // );
     console.log("Connected to MongoDB using Mongoose...");
     server = app.listen(port, () => {
       console.log(`Example app listening on port ${port}`);
